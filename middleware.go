b15a0b5a5568ec02f25c847c0ed6c5357559a138
ca11ou5 <ca11ou5@internet.ru>
2024-10-28 09:29:07 +0300

    fix

--- middleware.go
+++ middleware.go
@@ -18,7 +18,7 @@ func NeedRoles(requiredRoles ...string) Middleware {
 					code := r.URL.Query().Get("code")
 					if code == "" {
 						w.Header().Set("Referrer-Policy", "strict-origin")
-						w.Header().Set("Content-Type", "text/html")
+						w.Header().Set("Accept", "text/html")
 						http.Redirect(w, r, generateCodeURL(fmt.Sprintf("https://%s", r.Host)), http.StatusFound)
 						return
 					} else {
