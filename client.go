ef763b06f8b2807cfa2d4f36dae32c4f66cf92cb
ca11ou5 <ca11ou5@internet.ru>
2024-10-28 04:04:11 +0300

    fix: encoded url

--- client.go
+++ client.go
@@ -3,6 +3,7 @@ package keycloak
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 type Client struct {
@@ -20,5 +21,7 @@ func NewClient(baseURL string, clientID string, realm string, scope string) {
 }
 
 func generateCodeURL(redirectURL string) string {
-	return fmt.Sprintf("%sauth/realms/%s/protocol/openid-connect/auth?client_id=%s&redirect_uri=%s&response_type=code&scope=%s", keycloakClient.BaseURL, keycloakClient.Realm, keycloakClient.ClientID, redirectURL, keycloakClient.Scope)
+	encodedRedirectURL := url.QueryEscape(redirectURL)
+
+	return fmt.Sprintf("%sauth/realms/%s/protocol/openid-connect/auth?client_id=%s&redirect_uri=%s&response_type=code&scope=%s", keycloakClient.BaseURL, keycloakClient.Realm, keycloakClient.ClientID, encodedRedirectURL, keycloakClient.Scope)
 }
